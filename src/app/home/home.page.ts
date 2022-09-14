bf312700a3a2f0667a409b61e89c9cfb31bbd5af
aldaine-AG <aldaineclarke1@gmail.com>
2022-09-13 23:39:40 -0500

    initial commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { IonRouterOutlet, Platform } from '@ionic/angular';
+import { App } from '@capacitor/app';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+})
+export class HomePage implements OnInit {
+
+
+  constructor(
+    private platform: Platform,
+    private routerOutlet: IonRouterOutlet
+  ) {
+    this.platform.backButton.subscribeWithPriority(-1, () => {
+      if (!this.routerOutlet.canGoBack()) {
+        App.exitApp();
+      }
+    });
+  }
+
+  ngOnInit() {
+  }
+
+}
